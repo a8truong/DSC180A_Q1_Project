a8538013d1a246f98adb90229950263aba327dd9
a8truong <a8truong@ucsd.edu>
2024-12-03 20:26:22 -0800

    functional basic guardrails

--- app.py
+++ app.py
@@ -1,79 +1,142 @@
-# note: this is just the base code, no alterations have been made yet
-# creates the streamlit app with guardrails ai implemented (fllowing the tutorial)
-# TBD: exploring other guardrails with this same application
-
-from profanity_check import predict
-import guardrails as gd
-from guardrails.validators import Validator, EventDetail, register_validator
-from typing import Dict, List
-from rich import print
-import openai
 import streamlit as st 
-from dotenv import load_dotenv
 import os
+import openai
+from dotenv import load_dotenv
+from openai import OpenAI
+
+from trulens.core import Feedback, TruSession
+from trulens.core.guardrails.base import block_input, block_output
+#from trulens.apps.basic import TruBasicApp
+from trulens.providers.openai import OpenAI as OAI
+
+#import asyncio
+#import nest_asyncio
+
+# Apply nest_asyncio BEFORE importing any async libraries
+#nest_asyncio.apply()
+
+# Set up event loop explicitly
+#try:
+#    loop = asyncio.get_event_loop()
+#except RuntimeError:
+#    loop = asyncio.new_event_loop()
+#asyncio.set_event_loop(loop)
+
+
+#from nemoguardrails import RailsConfig, LLMRails
+
+#config = RailsConfig.from_path("config/config.yml")
+#rails = LLMRails(config)
 
 load_dotenv()
 
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+# os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 openai.api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI()
+
+oai_provider = OAI()
+
+f_hate = Feedback(
+    oai_provider.moderation_hate, higher_is_better=False
+).on_output()
+f_violent = Feedback(
+    oai_provider.moderation_violence, higher_is_better=False
+).on_output()
+f_selfharm = Feedback(
+    oai_provider.moderation_selfharm, higher_is_better=False
+).on_output()
+f_maliciousness = Feedback(
+    oai_provider.moderation_harassment_threatening, higher_is_better=False
+).on_output()
+f_sexual = Feedback(
+    oai_provider.moderation_sexual, higher_is_better=False
+).on_output()
+
+#feedbacks = [f_hate, f_violent, f_selfharm, f_maliciousness]
 
 def without_guardrails(text):
-    response = openai.Completion.create(
-        prompt="Translate the texts to English language\n"+text,
-        engine="text-davinci-003",
+    response = client.completions.create(
+        prompt="Translate the following text to English language:\n"+text,
+        model="gpt-3.5-turbo-instruct",
         max_tokens=2048,
         temperature=0)
 
-    result = response['choices'][0]['text']
+    result = response.choices[0].text.strip()
     return result
 
+def catch_pii(output: str):
+    res = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages = [{
+            "role": "system",
+            "content": """You are a PII detection bot. You will be given text to translate, and instead of translating it one-to-one, you need to find any PII and replace it with <PII_TYPE/>, where the 'PII_TYPE'
+              is replaced with the kind of PII being filtered out (for example, PHONE_NUMBER, FULL_NAME, etc...). If no PII is present, you can return it as-is."""
+        }, {
+            "role": "user",
+            "content": "Translate the texts to English language:\n"+output
+        }],
+        max_tokens=2048,
+        temperature=0
+    )
 
-rail_str = """
-<rail version="0.1">
+    return res.choices[0].message.content
 
-<script language='python'>
+def catch_profanity(output: str):
+    res = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages = [{
+            "role": "system",
+            "content": """You are a profanity detection bot. You will be given text to translate to English, but you need to first check if
+            the text contains profanity. If it does, you should respond with 'Flagged content: {text}' Otherwise, respond with the translated text
+            without any extra words"""
+        }, {
+            "role": "user",
+            "content": "Translate the texts to English language:\n"+output
+        }],
+        max_tokens=2048,
+        temperature=0
+    )
 
-@register_validator(name="is-profanity-free", data_type="string")
-class IsProfanityFree(Validator):
-    global predict
-    global EventDetail
-    def validate(self, key, value, schema) -> Dict:
-        text = value
-        prediction = predict([value])
-        if prediction[0] == 1:
-            raise EventDetail(
-                key,
-                value,
-                schema,
-                f"Value {value} contains profanity language",
-                "",
-            )
-        return schema
-</script>
+    return res.choices[0].message.content
 
-<output>
-    <string
-        name="translated_statement"
-        description="Translate the given statement into english language"
-        format="is-profanity-free"
-        on-fail-is-profanity-free="fix" 
-    />
-</output>
+#rails = [catch_pii_llms, catch_profanity]
+
+@block_input(feedback = f_selfharm,
+                threshold = 0.5,
+                return_value = "Sorry, I can't translate that.")
+@block_input(feedback = f_hate,
+                threshold = 0.5,
+                return_value = "Sorry, I can't translate that.")
+@block_input(feedback = f_violent,
+                threshold = 0.5,
+                return_value = "Sorry, I can't translate that.")
+@block_input(feedback = f_maliciousness,
+                threshold = 0.5,
+                return_value = "Sorry, I can't translate that.")
+@block_input(feedback = f_sexual,
+                threshold = 0.5,
+                return_value = "Sorry, I can't translate that.")
+def with_guardrails(text):
+    response = catch_profanity(text)
+    response = catch_pii(response)
 
 
-<prompt>
+    #response = rails.generate(messages=[{
+    #"role": "user",
+    #"content": f"Translate the following text: {text}"
+    #    }]).choices[0].message.content
+    #guard = Guard().use(
+    #CorrectLanguage(expected_language_iso="en", threshold=0.75))
 
-Translate the given statement into english language:
+    #guard.validate(response)
+    
+    return response
 
-{{statement_to_be_translated}}
-
-@complete_json_suffix
-</prompt>
-
-</rail>
-"""
-
-guard = gd.Guard.from_rail_string(rail_str)
+prompts = [
+    "お前の死ぬことがいいな",
+    "殺す",
+    "殴ってやる",
+]
 
 def main():
 
@@ -85,21 +148,13 @@ def main():
         if len(text_area)>0:
             st.info(text_area)
 
-            st.warning("Translation Without Guardrails")
-
+            st.warning("Translation Response Without Guardrails")
             without_guardrails_result = without_guardrails(text_area)
             st.success(without_guardrails_result)
 
             st.warning("Translation With Guardrails")
-
-            raw_llm_response, validated_response = guard(
-                openai.Completion.create,
-                prompt_params={"statement_to_be_translated": text_area},
-                engine="text-davinci-003",
-                max_tokens=2048,
-                temperature=0)
-
-            st.success(f"Validated Output: {validated_response}")
+            with_guardrails_result = with_guardrails(text_area)
+            st.success(with_guardrails_result)
 
 
 if __name__ == "__main__":
